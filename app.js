ba94ebe8f801fd0d599bf7b0502199b4ebab728d
TienNg21 <nghuutien0912@gmail.com>
2021-11-08 16:03:28 +0700

    add public folder

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ initializePassport(passport);
 const app = express();
 
 app.set("view engine", 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: false}));
 app.use(session({
     secret: 'secret',
