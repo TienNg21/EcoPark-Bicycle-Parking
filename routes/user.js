eb145d1f2a9f119d25d26ba6140c537de26ceb80
TienNg21 <nghuutien0912@gmail.com>
2021-11-09 22:48:12 +0700

    register link to table khach_hang

--- routes/user.js
+++ routes/user.js
@@ -22,11 +22,17 @@ userRouter.get("/register", (req, res) => {
 })
 
 userRouter.post("/register", async (req, res) => {
-    let { name, email, password, password2} = req.body;
+    let { name, email, addr, phone, lacudan, macudan, CMND, gender, password, password2} = req.body;
 
     console.log({
         name,
         email,
+        addr,
+        phone,
+        lacudan,
+        macudan,
+        CMND,
+        gender,
         password,
         password2
     });
@@ -51,22 +57,22 @@ userRouter.post("/register", async (req, res) => {
         // form validation has pass
 
         let hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
+        // console.log(hashedPassword);
 
         pool.query(
-            "SELECT * FROM users WHERE email = $1", [email], (err, results) => {
+            "SELECT * FROM khach_hang WHERE email = $1", [email], (err, results) => {
                
-                console.log(results);
+                // console.log(results.rows.length);
 
                 if(results.rows.length > 0){
                     errors.push({message: "Email này đã đăng ký với tài khoản khác."});
                     res.render("register", {errors: errors});
                 }else{
                     pool.query(
-                        `INSERT INTO users (name, email, password)
-                        VALUES ($1, $2, $3)
-                        RETURNING id, password`,
-                        [name, email, hashedPassword], (err, results) => {
+                        `INSERT INTO khach_hang (ten, email, dia_chi, sdt, la_cu_dan, ma_cu_dan, CMND, gioi_tinh, password)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+                        [name, email, addr, phone, lacudan, macudan, CMND, gender, hashedPassword],
+                        (err, results) => {
                             if(err){
                                 throw err;
                             }
