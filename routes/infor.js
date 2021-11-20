153a960e896c79a8edf163a87a700ed97e32348d
Quốc Phan Chính <Q10001001>
2021-11-20 17:43:20 +0700

    doi mk

--- routes/infor.js
+++ routes/infor.js
@@ -3,7 +3,11 @@ const inforRouter = express.Router();
 const { pool } = require("../dbConfig");
 const bcrypt = require("bcrypt");
 
+const blankerrors = []
+const blanksuccess = []
+
 let errors = [];
+let success = []
 
 inforRouter.get('/', (req, res) => {
     if(req.user == null){
@@ -11,7 +15,7 @@ inforRouter.get('/', (req, res) => {
     }
     console.log("view infor page");
     
-    var userInfor = req.user;
+    let userInfor = req.user;
     console.log(userInfor);
     res.render('infor.ejs', {
         ten: userInfor.ten,
@@ -22,7 +26,8 @@ inforRouter.get('/', (req, res) => {
         gioitinh: userInfor.gioi_tinh,
         lacudan: userInfor.la_cu_dan,
         macudan: userInfor.ma_cu_dan,
-        errors: errors
+        errors: blankerrors,
+        success: blanksuccess
     });
 })
 
@@ -30,6 +35,7 @@ inforRouter.post('/', async (req, res) => {
     var newUser = req.body;
     var phone = newUser.sdt;
     errors = [];
+    success = []
     var CMND = newUser.cmnd;
     var phone_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
     if(phone.length != 10 || phone_regex.test(phone) == false){
@@ -42,7 +48,19 @@ inforRouter.post('/', async (req, res) => {
     }
     if(errors.length > 0){
         // console.log(errors);
-        res.redirect("/infor");
+        // let userInfor = req.user;
+                res.render('infor.ejs', {
+                    ten: req.user.ten,
+                    email: req.user.email,
+                    diachi: req.user.dia_chi,
+                    sdt: req.user.sdt,
+                    cmnd: req.user.cmnd,
+                    gioitinh: req.user.gioi_tinh,
+                    lacudan: req.user.la_cu_dan,
+                    macudan: req.user.ma_cu_dan,
+                    errors: errors,
+                    success: success
+                });
     }else
         // errors = []
         pool.query(
@@ -54,16 +72,96 @@ inforRouter.post('/', async (req, res) => {
                 if(err){
                     throw err;
                 }
-                res.redirect("/infor");
+                success.push({message: 'Cap nhat thong tin thanh cong'})
+                // res.redirect("/infor");
+                // let userInfor = req.user;
+                res.render('infor.ejs', {
+                    ten: req.user.ten,
+                    email: req.user.email,
+                    diachi: req.user.dia_chi,
+                    sdt: req.user.sdt,
+                    cmnd: req.user.cmnd,
+                    gioitinh: req.user.gioi_tinh,
+                    lacudan: req.user.la_cu_dan,
+                    macudan: req.user.ma_cu_dan,
+                    errors: errors,
+                    success: success
+                });
                 
             }
         )
 })
 
-inforRouter.post('/password', (req, res)=>{
+inforRouter.post('/password', async (req, res)=>{
+    // res.send(req.body)
+    errors = []
     let {password, password1, password2} = req.body
-    let user = req.user
-    if(!password || !password1 || !password2) errors.push({message: 'Vui lòng điền đầy đủ mật khẩu'})
+    if(password1 !== password2){
+        errors.push({message: 'Mat khau xac nhan khong khop'})
+    }
+    if(password.length < 6 || password1.length < 6 || password2.length < 6){
+        errors.push({message: "Mật khẩu cần có ít nhất 6 ký tự."});
+    }
+    if(errors.length > 0){
+        // let userInfor = req.user;
+                res.render('infor.ejs', {
+                    ten: req.user.ten,
+                    email: req.user.email,
+                    diachi: req.user.dia_chi,
+                    sdt: req.user.sdt,
+                    cmnd: req.user.cmnd,
+                    gioitinh: req.user.gioi_tinh,
+                    lacudan: req.user.la_cu_dan,
+                    macudan: req.user.ma_cu_dan,
+                    errors: errors,
+                    success: success
+                });
+    }
+    else{
+        let curPass = req.user.password
+        let check = bcrypt.compareSync(password, curPass);
+        if(!check){
+            errors.push({message: 'Mat khau cu chua chinh xac'})
+            // let userInfor = req.user;
+            res.render('infor.ejs', {
+                ten: req.user.ten,
+                email: req.user.email,
+                diachi: req.user.dia_chi,
+                sdt: req.user.sdt,
+                cmnd: req.user.cmnd,
+                gioitinh: req.user.gioi_tinh,
+                lacudan: req.user.la_cu_dan,
+                macudan:req.user.ma_cu_dan,
+                errors: errors,
+                success: success
+            });
+        }
+        else{
+            let hashedPassword = await bcrypt.hash(password1, 10);
+            pool.query(
+                'update khach_hang set password = $1 where email = $2' ,
+                [hashedPassword, req.user.email],
+                (err, results)=>{
+                    if(err) throw err
+                    success.push({message: 'Doi mat khau'})
+                    // res.redirect('/infor')
+                    // let userInfor = req.user;
+                    res.render('infor.ejs', {
+                        ten: req.user.ten,
+                        email: req.user.email,
+                        diachi: req.user.dia_chi,
+                        sdt: req.user.sdt,
+                        cmnd: req.user.cmnd,
+                        gioitinh: req.user.gioi_tinh,
+                        lacudan: req.user.la_cu_dan,
+                        macudan:req.user.ma_cu_dan,
+                        errors: errors,
+                        success: success
+                    });
+                }
+            )
+        }
+    }
 
 })
 
