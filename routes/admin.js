525796336a4b0c211327361ae6a1d73abc57d2ff
TienNg21 <nghuutien0912@gmail.com>
2021-12-01 12:22:15 +0700

    fix tam thoi loi bat dong bo

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 const adminRouter = express.Router();
 const { pool } = require('../dbConfig');
 
-let xe = [];
-let baixe = [];
-let khachhang = [];
-let lsu = [];
-let price = {
+var xe;
+var baixe;
+var khachhang;
+var lsu;
+var price = {
     one: 50000,
     two: 70000,
     three: 10000,
@@ -16,11 +16,11 @@ let price = {
 adminRouter.get('/', async (req,res) => {
     if (req.user == null || req.user.email !== process.env.EMAIL_ADMIN){
         res.redirect('/login');
-    }else{
+    }else{        
         pool.query(
             'SELECT xe.id_xe, xe.id_user, bai_xe.ten_bai, xe.loai_xe, xe.trang_thai FROM xe, bai_xe WHERE xe.id_bai_xe = bai_xe.id_bai_xe',
             (err, results) => {
-                xe = await results.rows;
+                xe = results.rows;
             }
         );
         pool.query(
@@ -34,24 +34,26 @@ adminRouter.get('/', async (req,res) => {
             (err, results) => {
                 khachhang = results.rows;
             }
-        )
+        );
         pool.query(
             'SELECT * FROM lich_su_thue_xe',
             (err, results) => {
                 lsu = results.rows;
             }
-        )
-        pool.query(
-            'SELECT SUM(thanh_tien) FROM lich_su_thue_xe'
-        )
-        res.render('admintest.ejs', {
-            xe: xe,
-            baixe: baixe,
-            khachhang: khachhang,
-            lsu: lsu,
-            price: price
-        });
-        
+        );
+        function renderAdmin () {
+            res.render('admintest.ejs', {
+                xe: xe,
+                baixe: baixe,
+                khachhang: khachhang,
+                lsu: lsu,
+                price: price
+            });
+        }
+        setTimeout( function(){
+            renderAdmin();
+        }, 2000);
+        // renderAdmin();
     }
     
 })
